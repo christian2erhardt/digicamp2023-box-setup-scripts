4cb205afe418fb3c22c9404b00e728ee765cc854
Christian Erhardt <christian.erhardt@mojo2k.de>
2023-06-01 09:05:19 +0200

    fix: not

--- scripts/M5StackTools.ps1
+++ scripts/M5StackTools.ps1
@@ -21,7 +21,7 @@ pip install matplotlib
 choco install -y vcredist2015
 
 # Clone Repo
-if (Test-Path -Path Core2-for-AWS-IoT-EduKit -eq $false) {
+if (-not (Test-Path -Path Core2-for-AWS-IoT-EduKit)) {
   git clone https://github.com/m5stack/Core2-for-AWS-IoT-EduKit.git
 }
 
@@ -48,8 +48,7 @@ foreach ($ext in $extensions) {
 # Serial UART Driver
 #Start-BitsTransfer -Source "https://www.silabs.com/documents/public/software/CP210x_Universal_Windows_Driver.zip" -Destination CP210x_Universal_Windows_Driver.zip
 
-$a = Test-Path -Path CP210x_Universal_Windows_Driver
-if ($a -eq $false) {
+if (-not (Test-Path -Path CP210x_Universal_Windows_Driver)) {
   Invoke-WebRequest -Uri https://www.silabs.com/documents/public/software/CP210x_Universal_Windows_Driver.zip -OutFile CP210x_Universal_Windows_Driver.zip -UseBasicParsing
   7z x CP210x_Universal_Windows_Driver.zip -o*
   cd CP210x_Universal_Windows_Driver
@@ -61,16 +60,14 @@ cd $env:USERPROFILE\desktop\DigiCamp2023
 
 # M5 Burner
 #Start-BitsTransfer -Source "https://m5burner.m5stack.com/app/M5Burner-v3-beta-win-x64.zip" -Destination M5Burner-v3-beta-win-x64.zip
-$a = Test-Path -Path M5Burner-v3-beta-win-x64
-if ($a -eq $false) {
+if (-not (Test-Path -Path M5Burner-v3-beta-win-x64)) {
   Invoke-WebRequest -Uri https://m5burner.m5stack.com/app/M5Burner-v3-beta-win-x64.zip -OutFile M5Burner-v3-beta-win-x64.zip -UseBasicParsing
   7z x M5Burner-v3-beta-win-x64.zip -o*
   del M5Burner-v3-beta-win-x64.zip
 }
 
 # UIFlow
-$a = Test-Path -Path UIFlow-Desktop-IDE
-if ($a -eq $false) {
+if (-not (Test-Path -Path UIFlow-Desktop-IDE)) {
   #Start-BitsTransfer -Source "https://m5stack.oss-cn-shenzhen.aliyuncs.com/resource/software/UIFlow-Desktop-IDE.zip" -Destination UIFlow-Desktop-IDE.zip
   Invoke-WebRequest -Uri https://m5stack.oss-cn-shenzhen.aliyuncs.com/resource/software/UIFlow-Desktop-IDE.zip -OutFile UIFlow-Desktop-IDE.zip -UseBasicParsing
   7z x UIFlow-Desktop-IDE.zip -o*
